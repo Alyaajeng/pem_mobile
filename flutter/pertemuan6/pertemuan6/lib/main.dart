c014dc920842ced7c17bd235076fcd493f2ad751
Alyaajeng <alyaajeng29@gmail.com>
2025-10-04 00:04:14 +0700

    P6 Dasar Widget -Container Padding & Margin

--- flutter/pertemuan6/pertemuan6/lib/main.dart
+++ flutter/pertemuan6/pertemuan6/lib/main.dart
@@ -10,8 +10,7 @@ class AlyaApp extends StatelessWidget {
     return MaterialApp(
       title: "AlyaApp",
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Column & Row Example")),
+        appBar: AppBar(title: Text("Column & Row Example")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -26,6 +25,15 @@ class AlyaApp extends StatelessWidget {
                 ],
               ),
               const Text("Bawah"),
+              Container(
+                margin: const EdgeInsets.all(20), // jarak luar
+                padding: const EdgeInsets.all(10), // jarak dalam
+                color: Colors.blue,
+                child: const Text(
+                  "Dengan Margin & Padding",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
             ],
           ),
         ),
@@ -34,46 +42,29 @@ class AlyaApp extends StatelessWidget {
             padding: EdgeInsets.zero,
             children: const [
               DrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
+                decoration: BoxDecoration(color: Colors.blue),
                 child: Text(
                   'Drawer Header',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 24,
-                  ),
+                  style: TextStyle(color: Colors.white, fontSize: 24),
                 ),
               ),
-              ListTile(
-                leading: Icon(Icons.message),
-                title: Text('Messages'),
-              ),
+              ListTile(leading: Icon(Icons.message), title: Text('Messages')),
               ListTile(
                 leading: Icon(Icons.account_circle),
                 title: Text('Profile'),
               ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-            ], 
+              ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+            ],
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
           items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
             BottomNavigationBarItem(
               icon: Icon(Icons.business),
               label: 'Business',
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School',
-            ),
+            BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
           ],
         ),
       ),
