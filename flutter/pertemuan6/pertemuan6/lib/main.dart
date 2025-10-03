4ec34a8c68121c8fe3f9bb4959d7d6d7c9947c5a
Alyaajeng <alyaajeng29@gmail.com>
2025-10-03 22:19:51 +0700

    P6 - Dasar Widget BottomNavigationBar

--- flutter/pertemuan6/pertemuan6/lib/main.dart
+++ flutter/pertemuan6/pertemuan6/lib/main.dart
@@ -56,6 +56,22 @@ class AlyaApp extends StatelessWidget {
             ], 
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.business),
+              label: 'Business',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.school),
+              label: 'School',
+            ),
+          ],
+        ),
       ),
     );
   }
