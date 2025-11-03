d0243dc5fbf2aeb0edc60dbf91d2f27b10aa56a7
Alyaajeng <alyaajeng29@gmail.com>
2025-11-04 00:12:55 +0700

    NAVIGASI DAN ROUTING_Contoh Navigator

--- flutter/pertemuan7/lib/main.dart
+++ flutter/pertemuan7/lib/main.dart
@@ -1,35 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(StylingExample());
+  runApp(HomePage());
 }
 
-class StylingExample extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Styling and Positioning'),
+          title: Text('Halaman Utama'),
         ),
-        body: Stack(
-          children: [
-            Container(color: Colors.lightBlueAccent),
-            Align(
-              alignment: Alignment.center,
-              child: Container(
-                padding: const EdgeInsets.all(20),
-                color: Colors.blue,
-                child: Text(
-                  'Tengah Layar',
-                  style: TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ),
-          ],
+        body: Center(
+          child: ElevatedButton(
+            child: Text('Ke Halaman Kedua'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondPage()),
+              );
+            },
+          )
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halaman Kedua'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Kembali'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
         )
       ),
     );
