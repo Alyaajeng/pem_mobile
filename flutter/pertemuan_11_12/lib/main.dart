b52b6532b722b186cf90aae9b4dc3fa61c91b5d9
Alyaajeng <alyaajeng29@gmail.com>
2025-11-20 11:15:33 +0700

    P11 - Bagian 1: Pengenalan Konsep Arsitektur Model-View

--- flutter/pertemuan_11_12/lib/main.dart
+++ flutter/pertemuan_11_12/lib/main.dart
@@ -1,122 +1,185 @@
 import 'package:flutter/material.dart';
+import 'package:pertemuan_11_12/model.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class _MyAppState extends State<MyApp> {
+  AppSettings settings = AppSettings();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void toggleTheme() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      settings.isDarkMode = !settings.isDarkMode;
+    });
+  }
+
+  void updateName(String newName) {
+    setState(() {
+      settings.username = newName;
+    });
+  }
+
+  void decreaseFont() {
+    setState(() {
+      if (settings.fontSize > 8) {
+        settings.fontSize -= 2;
+      }
+    });
+  }
+
+  void increaseFont() {
+    setState(() {
+      settings.fontSize += 2;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      // Mengatur tema aplikasi secara global
+      theme: settings.isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: settings.backgroundColor,
+        appBar: AppBar(
+          title: Text('Aplikasi Settings Sederhana', style: TextStyle(color: Colors.white)),
+          backgroundColor: settings.isDarkMode ? Colors.grey[900] : Colors.blue,
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(20),
+          child: ListView( // Menggunakan ListView agar dapat digulir jika layar kecil
+            children: [
+              // --- Bagian Sambutan dengan Ukuran Font Dinamis ---
+              Text(
+                settings.greeting,
+                style: TextStyle(
+                  fontSize: settings.fontSize.toDouble(),
+                  color: settings.textColor,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 30),
+
+              // --- Kontrol Mode Gelap/Terang ---
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: settings.isDarkMode ? Colors.grey[800] : Colors.blue[50],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Mode : ${settings.isDarkMode ? 'Gelap' : 'Terang'}',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: settings.textColor,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Switch(
+                      value: settings.isDarkMode,
+                      onChanged: (value) => toggleTheme(),
+                      activeColor: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              // --- INPUT NAMA ---
+              TextField(
+                onChanged: updateName,
+                decoration: InputDecoration(
+                  labelText: 'Ubah Nama',
+                  labelStyle: TextStyle(color: settings.textColor),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  filled: true,
+                  fillColor: settings.isDarkMode ? Colors.grey[800] : Colors.grey[100],
+                ),
+                style: TextStyle(color: settings.textColor),
+              ),
+
+              SizedBox(height: 20),
+
+              // --- UKURAN FONT ---
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: settings.isDarkMode ? Colors.blueGrey[800] : Colors.blue[50],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Ukuran Font: ${settings.fontSize}',
+                      style: TextStyle(
+                        color: settings.textColor,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        IconButton(
+                          onPressed: decreaseFont,
+                          icon: Icon(Icons.remove_circle, color: settings.textColor),
+                          tooltip: 'Perkecil Font',
+                        ),
+                        SizedBox(width: 20),
+                        IconButton(
+                          onPressed: increaseFont,
+                          icon: Icon(Icons.add_circle, color: settings.textColor),
+                          tooltip: 'Perbesar Font',
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              // --- PREVIEW TEXT ---
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  border: Border.all(color: settings.textColor.withOpacity(0.3)),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Text(
+                  'Ini adalah preview text dengan ukuran font ${settings.fontSize}',
+                  style: TextStyle(
+                    fontSize: settings.fontSize.toDouble(),
+                    color: settings.textColor,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ],
+          ),
+        ),
+        // --- Floating Action Button ---
+        floatingActionButton: FloatingActionButton(
+          onPressed: toggleTheme,
+          child: Icon(settings.isDarkMode ? Icons.light_mode : Icons.dark_mode, color: Colors.white),
+          backgroundColor: settings.isDarkMode ? Colors.blueGrey[800] : Colors.blue,
+          tooltip: 'Ganti Tema',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
